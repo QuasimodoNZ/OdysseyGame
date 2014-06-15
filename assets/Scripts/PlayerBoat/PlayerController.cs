436054d30f6065d765d85ac32362cd6273912d1f
Benjamin Riddell <benjamin.riddell@hotmail.com>
2014-06-15 16:10:08 +1200

    Fixing formatting and getting scene ready for merge

--- assets/Scripts/PlayerBoat/PlayerController.cs
+++ assets/Scripts/PlayerBoat/PlayerController.cs
@@ -19,117 +19,121 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public int startingHp = 50; // Current and Full Hp the character starts with
-	private bool dead = false;
+		public int startingHp = 50; // Current and Full Hp the character starts with
+		private bool dead = false;
 	
-	// Current HP Stats
-	private int fullHp; // Current size of full hp bar. While this is currently just startingHp it can be modified during game
-	private int hp;	// Player's current hit points
-	private int toBeHealedAmt = 0;
-	private int healCounter = 0;
-	private int fullHealCounter = 1000;
-	public int healSpeed = 10;
-	public int healingHoleHealAmount = 15;
-	private bool isHealing = false;
+		// Current HP Stats
+		private int fullHp; // Current size of full hp bar. While this is currently just startingHp it can be modified during game
+		private int hp;	// Player's current hit points
+		private int toBeHealedAmt = 0;
+		private int healCounter = 0;
+		private int fullHealCounter = 1000;
+		public int healSpeed = 10;
+		public int healingHoleHealAmount = 15;
+		private bool isHealing = false;
 	
-	// Damage
-	public int cyclopsClubDamage = 20;
-	public int circeAttackDamage = 10;
+		// Damage
+		public int cyclopsClubDamage = 20;
+		public int circeAttackDamage = 10;
 	
-	// Score
-	private int score = 0; // Player's current
+		// Score
+		private int score = 0; // Player's current
 	
-	//For controlling the animation state of the ship
-	private AnimController animController;
+		//For controlling the animation state of the ship
+		private AnimController animController;
 	
-	// Use this for initialization
-	void Start ()
-	{
-		animController = GameObject.FindGameObjectWithTag (Tags.animationController).GetComponent<AnimController> ();
-		fullHp = startingHp;
-		hp = startingHp;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if ((toBeHealedAmt > 0) && !dead) {
-			isHealing = true;
-			healCounter += healSpeed;
-			if (healCounter >= fullHealCounter) {
-				toBeHealedAmt -= 1;
-				hp += 1;
-				healCounter = 0;
-				if (hp > fullHp)
-					hp = fullHp;
-			}
-		} else {
-			isHealing = false;
-			healCounter = 0;
+		// Use this for initialization
+		void Start ()
+		{
+				animController = GameObject.FindGameObjectWithTag (Tags.animationController).GetComponent<AnimController> ();
+				fullHp = startingHp;
+				hp = startingHp;
 		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if ((toBeHealedAmt > 0) && !dead) {
+						isHealing = true;
+						healCounter += healSpeed;
+						if (healCounter >= fullHealCounter) {
+								toBeHealedAmt -= 1;
+								hp += 1;
+								healCounter = 0;
+								if (hp > fullHp)
+										hp = fullHp;
+						}
+				} else {
+						isHealing = false;
+						healCounter = 0;
+				}
 		
-		animController.SetHealth (GetHpPercent ());
-		if (!dead) {
-			score = (int)transform.position.magnitude;
-		}
+				animController.SetHealth (GetHpPercent ());
+				if (!dead) {
+						score = (int)transform.position.magnitude;
+				}
 		
-	}
-	
-	public int GetHp ()
-	{
-		return hp;
-	}
-	
-	public int GetHpPercent ()
-	{	// Get HP as an integer percentage of full hp
-		if (hp <= 0) {
-			return 0;
 		}
-		return (int)(hp * 100 / fullHp);
-	}
 	
-	public void CyclopsClubDamage(){
-		InflictDamage (cyclopsClubDamage);
-	}
+		public int GetHp ()
+		{
+				return hp;
+		}
+	
+		public int GetHpPercent ()
+		{	// Get HP as an integer percentage of full hp
+				if (hp <= 0) {
+						return 0;
+				}
+				return (int)(hp * 100 / fullHp);
+		}
+	
+		public void CyclopsClubDamage ()
+		{
+				InflictDamage (cyclopsClubDamage);
+		}
 
-	public void CirceAttackDamage(){
-		InflictDamage (circeAttackDamage);
-	}
-	
-	public void HealingHoleEffect(){
-		HealPlayer (healingHoleHealAmount);
-		
-	}
-	
-	public void HealPlayer(int amount){
-		toBeHealedAmt += amount;
-		if ((toBeHealedAmt + hp) > fullHp) {
-			toBeHealedAmt = fullHp-hp;
+		public void CirceAttackDamage ()
+		{
+				InflictDamage (circeAttackDamage);
 		}
-		
-	}
 	
-	public void InflictDamage (int damage)
-	{ // Call whenever damage should be inflicted to player from any source
-		if (isHealing)
-			return;
-		hp -= damage;
+		public void HealingHoleEffect ()
+		{
+				HealPlayer (healingHoleHealAmount);
 		
-		if (hp <= 0) {
-			KillPlayer ();
 		}
-	}
 	
-	public void KillPlayer ()
-	{	// Call when the player is killed		
-		hp = 0;
-		dead = true;
-		gameObject.GetComponent<PlayerMovement> ().SetSinking ();
-		animController.SetDeath (true);
-	}
+		public void HealPlayer (int amount)
+		{
+				toBeHealedAmt += amount;
+				if ((toBeHealedAmt + hp) > fullHp) {
+						toBeHealedAmt = fullHp - hp;
+				}
+		
+		}
 	
-	public int GetScore ()
-	{
-		return score;
-	}
+		public void InflictDamage (int damage)
+		{ // Call whenever damage should be inflicted to player from any source
+				if (isHealing)
+						return;
+				hp -= damage;
+		
+				if (hp <= 0) {
+						KillPlayer ();
+				}
+		}
+	
+		public void KillPlayer ()
+		{	// Call when the player is killed		
+				hp = 0;
+				dead = true;
+				gameObject.GetComponent<PlayerMovement> ().SetSinking ();
+				animController.SetDeath (true);
+		}
+	
+		public int GetScore ()
+		{
+				return score;
+		}
 }
