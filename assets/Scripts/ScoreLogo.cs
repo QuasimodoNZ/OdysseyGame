16808a40a58772e8dc5196e6c2a2f59c33185ac5
Thomas Gowing <thomasgowing@yahoo.co.nz>
2014-06-16 13:01:15 +1200

    Skybox changes, symbol added to GUI.

---
+++ assets/Scripts/ScoreLogo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreLogo : MonoBehaviour {
+
+	public Texture2D logo;
+	public float width;
+	public float height;
+	public float screenPercentW;
+	public float screenPercentH;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.DrawTexture (new Rect (Screen.width * screenPercentW, Screen.height * screenPercentH, width, height), logo);
+	}
+}
